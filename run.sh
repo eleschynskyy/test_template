7bb2a08d85f5c2f81a7718a8821e47e7548facf1
Yevhen.Leshchynskyy@hermes-europe.co.uk <yevhen.leshchynskyy@evri.com>
2026-03-13 15:45:18 +0200

    Adds parameters passing

--- run.sh
+++ run.sh
@@ -12,7 +12,6 @@ parse_input_variables() {
   for p in "${pairs[@]}"; do
     result+=" -J$p"
   done
-
   echo "${result# }"
 }
 
@@ -20,9 +19,7 @@ execute_test() {
     echo "Run jmeter test"
     TEST_DIR=$BASE_DIR/tests
     RESULTS_DIR=$BASE_DIR/results
-    parse_input_variables $variables
-
-    jmeter -n -t $TEST_DIR/test.jmx -l $RESULTS_DIR/test.jtl
+    jmeter -n -t $TEST_DIR/test.jmx -l $RESULTS_DIR/test.jtl $(parse_input_variables "$variables")
     cat $RESULTS_DIR/test.jtl
 }
 
